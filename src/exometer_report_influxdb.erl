f69e652df197a3fdcb514eb4d2d6ab14cc2d37c3
GalaxyGorilla <skattelmann@gmail.com>
2015-10-30 17:04:48 +0100

    added debug print

--- src/exometer_report_influxdb.erl
+++ src/exometer_report_influxdb.erl
@@ -84,9 +84,9 @@ exometer_report(Metric, DataPoint, Extra, Value,
                          state()) -> callback_result().
 exometer_subscribe(Metric, _DataPoint, Interval, Tags, 
                    #state{metrics=Metrics} = State) ->
-
     {MetricName, NewTags} = evaluate_subscription_tags(Metric, Tags),
     io:format("~p~n~p~n~p~n", [MetricName, NewTags, Interval]),
+    timer:sleep(1000),
     case MetricName of
         [] -> exit(invalid_metric_name);
         _  -> 
